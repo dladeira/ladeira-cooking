f98e347d9729c78059a45e5156faadacf515aa38
dladeira <dladeiratx@gmail.com>
2022-02-01 21:00:42 +0100

    Added page titles

--- components/layout.js
+++ components/layout.js
@@ -1,8 +1,13 @@
+import Head from 'next/head'
+
 import Navbar from './navbar.js'
 
 function Component({ children }) {
     return (
         <div style={{ height: "100%" }}>
+            <Head>
+                <title>Ladeira Cooking</title>
+            </Head>
             <Navbar />
             <div style={{ height: "100%", paddingTop: "6vh"}}>
                 {children}
