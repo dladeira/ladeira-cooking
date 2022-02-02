9ea38452e62b6a84938c2a27ad9af76f65d6c230
dladeira <dladeiratx@gmail.com>
2022-02-02 11:21:51 +0100

    Added ingredients
    closes #2

--- models/recipe.js
+++ models/recipe.js
@@ -2,9 +2,11 @@ import mongoose from 'mongoose'
 
 const schema = new mongoose.Schema({
     iconPath: String,
+    splashPath: String,
     id: String,
     title: String,
-    content: String
+    steps: Array,
+    ingredients: Array,
 })
 
 module.exports = mongoose.models.Recipe || mongoose.model("Recipe", schema)
--- pages/recipes/[id].js
+++ pages/recipes/[id].js
@@ -4,25 +4,44 @@ import Recipe from '../../models/recipe.js'
 
 import styles from '../../styles/recipe.module.scss'
 
-function Page({ title, steps, splashPath }) {
+function Page({ title, steps, splashPath, ingredients }) {
 
     return (
         <div className={styles.container}>
-            <RecipeComponent title={title} steps={steps} splashPath={splashPath} />
+            <RecipeComponent title={title} steps={steps} splashPath={splashPath} ingredients={ingredients} />
         </div>
     )
 }
 
-function RecipeComponent({ title, steps, splashPath }) {
+function RecipeComponent({ title, steps, splashPath, ingredients }) {
     return (
         <div className={styles.recipeContainer}>
             <div className={styles.title}>{title}</div>
             <div className={styles.line} />
             <div className={styles.landingImage}><Image alt={"Icon"} src={splashPath} height={400} width={800} /></div>
-            <div className={styles.content}>{steps.map(step => {
-                return <Step key={steps.indexOf(step)} number={steps.indexOf(step)} text={step.text} image={step.image} />
-            })}</div>
+            <div className={styles.content}>
 
+                <Ingredients ingredients={ingredients} />
+
+                {steps.map(step => {
+                    return <Step key={steps.indexOf(step)} number={steps.indexOf(step)} text={step.text} image={step.image} />
+                })}
+            </div>
+
+        </div>
+    )
+}
+
+function Ingredients({ ingredients }) {
+    return (
+        <div className={styles.ingContainer}>
+            <div className={styles.ingTitle}>Ingredients</div>
+
+            <div className={styles.ings}>
+                {ingredients.map(ing => {
+                    return <div key={ingredients.indexOf(ing)} className={styles.ing}>- {ing}</div>
+                })}
+            </div>
         </div>
     )
 }
